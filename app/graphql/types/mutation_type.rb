60a07fd7c6f334dab983a7f55a66f5b598c18da4
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-11-13 18:26:45 +0900

    user作成のgraphql実装

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :add_user, mutation: Mutations::AddUser
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
